b292b84e13a720201d29666e78a1eea0cdf16034
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2025-03-19 19:50:52 +0530

    first commit

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb+srv://23ee01025:Krishnendu987@cluster0.razwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/accutax";
-
+const mongoURI="mongodb+srv://23ee01025:Krishnendu@cluster0.razwk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const ConnectToMongo=()=>{
     mongoose.connect(mongoURI,).then(()=>console.log("connected"))
 }
